30c39649c6587f344378d9c4b9de59e44f358822
RV-01 <bachhavroshan600@gmail.com>
2022-12-13 14:00:51 +0530

    initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => (runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar : AppBar(
+          title:Center( child: Text("Hi"),
+        ),
+          backgroundColor: Colors.green[500],
+      ),body:  Center(
+            child :Image.asset("images/diamond2.png")
+        ),backgroundColor: Colors.blueGrey,
+        ),
+
+),
+// ),
+)
+);
